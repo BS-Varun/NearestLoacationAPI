3870d9027ac8069b1f3db1d5739799bdfdb0be46
B S Varun <bsvarun31@gmail.com>
2023-06-24 19:49:46 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,52 @@
 const express = require('express');
-const coordinateRoutes = require('./routes/coordinate');
+const NodeCache = require('node-cache');
+const { findNearestLocation } = require('./utils/location');
+const { validateCoordinates } = require('./utils/validation');
 
 const app = express();
-const PORT = 3002;
+const cache = new NodeCache({ stdTTL: 60 }); // Cache TTL set to 60 seconds
 
-app.use(express.json());
+// API endpoint to find the nearest location
+app.get('/nearest-location', (req, res) => {
+  const { latitude, longitude } = req.query;
 
+  // Validate input coordinates
+  const validationErrors = validateCoordinates(latitude, longitude);
+  if (validationErrors.length > 0) {
+    return res.status(400).json({ errors: validationErrors });
+  }
 
-app.use('/coordinate', coordinateRoutes);
+  // Generate cache key based on coordinates
+  const cacheKey = `${latitude}-${longitude}`;
 
-//Error handling Middleware
-app.use((err, req, res, next) => {
-  console.error(err); // Log the error for debugging purposes
-  res.status(500).json({ error: 'Internal server error' });
+  try {
+    // Check if the result is already in cache
+    const cachedResult = cache.get(cacheKey);
+    if (cachedResult) {
+      return res.json(cachedResult);
+    }
+
+    // Convert coordinates to numbers
+    const targetLatitude = parseFloat(latitude);
+    const targetLongitude = parseFloat(longitude);
+
+    const nearestLocation = findNearestLocation(targetLatitude, targetLongitude);
+
+    if (!nearestLocation) {
+      return res.status(404).json({ error: 'No nearest location found' });
+    }
+
+    // Store the result in cache
+    cache.set(cacheKey, nearestLocation);
+
+    res.json(nearestLocation);
+  } catch (error) {
+    console.error('Error occurred:', error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is listening on port ${PORT}`);
+// Start the server
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
 });
